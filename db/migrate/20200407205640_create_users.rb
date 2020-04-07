efc24c76f5b41b686d3d3c560ea2c15e16ad90ec
reireitube <reireitube@gmail.com>
2020-04-08 07:06:08 +0900

    webpack problem clear

---
+++ db/migrate/20200407205640_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+
+      t.string "name", null: false
+      t.string "mail", null: false
+      t.string "profile"
+      t.string "password_digest"
+      t.string "bandmember"
+      t.string "activity_base"
+      t.integer "my_money"
+      t.timestamps
+    end
+  end
+end
